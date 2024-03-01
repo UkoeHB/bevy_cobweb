14db08bf4586516a25fd55f729e3fb20e52b23ed
koe <ukoe@protonmail.com>
2024-02-29 23:19:44 -0600

    test reactor modes with despawn triggers

--- tests/test/react/reactor_mode.rs
+++ tests/test/react/reactor_mode.rs
@@ -18,22 +18,48 @@ fn persistent_reactor_lives_without_triggers()
     app.add_plugins(ReactPlugin);
     let world = &mut app.world;
 
-    // register reactor without any triggers
+    // register reactor
     let sys_command = world.syscall((),
         |mut rc: ReactCommands|
         {
             rc.on_persistent((), ||{})
         }
     );
-    reaction_tree(world);
 
-    // reactor should be alive
+    // reactor should not be garbage collected
+    assert!(world.get_entity(*sys_command).is_some());
+    reaction_tree(world);
     assert!(world.get_entity(*sys_command).is_some());
 }
 
 //-------------------------------------------------------------------------------------------------------------------
 
 // persistent: reactor not despawned when all despawn triggers fire
+#[test]
+fn persistent_reactor_lives_with_despawn_triggers_finished()
+{
+    // setup
+    let mut app = App::new();
+    app.add_plugins(ReactPlugin);
+    let world = &mut app.world;
+
+    // prep target entity
+    let target = world.spawn_empty().id();
+
+    // register reactor
+    let sys_command = world.syscall((),
+        move |mut rc: ReactCommands|
+        {
+            rc.on_persistent(despawn(target), ||{})
+        }
+    );
+
+    // reactor should not be garbage collected
+    assert!(world.get_entity(*sys_command).is_some());
+    world.despawn(target);
+    reaction_tree(world);
+    assert!(world.get_entity(*sys_command).is_some());
+}
 
 //-------------------------------------------------------------------------------------------------------------------
 
@@ -54,7 +80,7 @@ fn cleanup_reactor_dies_without_triggers()
     app.add_plugins(ReactPlugin);
     let world = &mut app.world;
 
-    // register reactor without any triggers
+    // register reactor
     let sys_command = world.syscall((),
         |mut rc: ReactCommands|
         {
@@ -73,6 +99,33 @@ fn cleanup_reactor_dies_without_triggers()
 //-------------------------------------------------------------------------------------------------------------------
 
 // cleanup: reactor despawned when all despawn triggers fire
+#[test]
+fn cleanup_reactor_dies_with_despawn_triggers_finished()
+{
+    // setup
+    let mut app = App::new();
+    app.add_plugins(ReactPlugin);
+    let world = &mut app.world;
+
+    // prep target entity
+    let target = world.spawn_empty().id();
+
+    // register reactor
+    let sys_command = world.syscall((),
+        move |mut rc: ReactCommands|
+        {
+            let sys_command = rc.commands().spawn_system_command(||{});
+            rc.with(despawn(target), sys_command, ReactorMode::Cleanup);
+            sys_command
+        }
+    );
+
+    // reactor should be garbage collected
+    assert!(world.get_entity(*sys_command).is_some());
+    world.despawn(target);
+    reaction_tree(world);
+    assert!(world.get_entity(*sys_command).is_none());
+}
 
 //-------------------------------------------------------------------------------------------------------------------
 
@@ -89,7 +142,7 @@ fn revokable_reactor_dies_without_triggers()
     app.add_plugins(ReactPlugin);
     let world = &mut app.world;
 
-    // register reactor without any triggers
+    // register reactor
     let token = world.syscall((),
         |mut rc: ReactCommands|
         {
@@ -106,6 +159,31 @@ fn revokable_reactor_dies_without_triggers()
 //-------------------------------------------------------------------------------------------------------------------
 
 // revokable: reactor despawned when all despawn triggers fire
+#[test]
+fn revokable_reactor_dies_with_despawn_triggers_finished()
+{
+    // setup
+    let mut app = App::new();
+    app.add_plugins(ReactPlugin);
+    let world = &mut app.world;
+
+    // prep target entity
+    let target = world.spawn_empty().id();
+
+    // register reactor
+    let token = world.syscall((),
+        move |mut rc: ReactCommands|
+        {
+            rc.on_revokable(despawn(target), ||{})
+        }
+    );
+
+    // reactor should be garbage collected
+    assert!(world.get_entity(*SystemCommand::from(token.clone())).is_some());
+    world.despawn(target);
+    reaction_tree(world);
+    assert!(world.get_entity(*SystemCommand::from(token)).is_none());
+}
 
 //-------------------------------------------------------------------------------------------------------------------
 
